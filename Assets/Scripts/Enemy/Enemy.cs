4c906e7947d827ec81efb5c7f8168b885c3f6b6d
Juan Martin <juanmartincas@gmail.com>
2021-02-27 18:18:11 -0500

    Alien enemy

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+
+    [Header("Components")]
+    [SerializeField] protected Transform _startPos; 
+    [SerializeField] protected Transform _endPos; 
+    [SerializeField] protected Transform _weaponPos; 
+    protected bool _canMove = true; 
+    protected Transform _currentTargetPos; 
+    protected Transform _parentTransform; 
+
+    [Space(5)]
+
+    [Header("Characteristics")]
+    [SerializeField] protected float _velocity = 10.0f;
+    [SerializeField] protected float _damage = 1.0f; 
+    [SerializeField] protected float _fireRate = 0.5f; 
+    [SerializeField] protected float _health = 1.0f; 
+
+    [Space(5)]
+
+    [Header("Target")]
+    [SerializeField] protected Transform _target;  
+
+    [Space(5)]
+
+    [Header("Target")]
+    [SerializeField] protected GameObject _bullet; 
+    public abstract void Move();
+    public abstract void Attack(); 
+    public abstract IEnumerator AttackCounter(); 
+}
---
+++ Assets/Scripts/Enemy/Ovni.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ovni : Enemy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentTargetPos = _startPos; 
+        _parentTransform = this.transform.parent.transform; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move(); 
+    }
+
+    public override void Move(){
+        float delta = _velocity * Time.deltaTime; 
+        _parentTransform.position = Vector3.MoveTowards(_parentTransform.position, _currentTargetPos.position, delta); 
+        if(Vector3.Distance(_parentTransform.position, _currentTargetPos.position) < 0.001f){
+            //change target
+            if(_currentTargetPos == _startPos){
+                _currentTargetPos = _endPos; 
+                return; 
+            }
+
+            _currentTargetPos = _startPos; 
+        }
+    }
+
+    public override void Attack()
+    {
+
+    }
+
+    public override IEnumerator AttackCounter()
+    {   
+        Attack(); 
+        yield return new WaitForSeconds(_fireRate); 
+    }
+}
