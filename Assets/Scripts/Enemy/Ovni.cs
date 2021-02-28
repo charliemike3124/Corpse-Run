155d934020ba9a789e3044f09874e48d2b8f6296
Juan Martin <juanmartincas@gmail.com>
2021-02-28 13:07:05 -0500

    Added meshes

--- Assets/Scripts/Enemy/Ovni.cs
+++ Assets/Scripts/Enemy/Ovni.cs
@@ -1,35 +1,46 @@
 ﻿
+using DG.Tweening;
 using UnityEngine;
 
+public class Ovni : Enemy {
+    [Header ("Mesh")]
+    [SerializeField] private Transform _meshTransform;
 
-public class Ovni : Enemy
-{
+    [Header ("Animation parameters")]
+    [SerializeField] private float _velAnim = 0.6f;
+    [SerializeField] private float _rotationAnimSmoothness = 0.5f;
+    private float _yRot = 0;
+    private Vector3 _actualRot = new Vector3 (0, 360, 0);
     // Start is called before the first frame update
-    void Start()
-    {
-        _currentTargetPos = _startPos; 
-        _parentTransform = this.transform.parent.transform; 
+    void Start () {
+        DOTween.Init ();
+        _currentTargetPos = _startPos;
+        _parentTransform = this.transform.parent.transform;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        Move(); 
-        if(_canAttack)
-            StartCoroutine(Attack()); 
+    void Update () {
+        RotateAnim ();
+        Move ();
+        if (_canAttack)
+            StartCoroutine (Attack ());
     }
 
-    public override void Move(){
-         float delta = _moveVelocity * Time.deltaTime; 
-        _parentTransform.position = Vector3.MoveTowards(_parentTransform.position, _currentTargetPos.position, delta); 
-        if(Vector3.Distance(_parentTransform.position, _currentTargetPos.position) < 0.001f){
+    private void RotateAnim () {
+        _meshTransform.DOLocalRotate (_actualRot, _rotationAnimSmoothness).SetLoops (-1, LoopType.Incremental).SetEase(Ease.Linear);
+    }
+
+    public override void Move () {
+        float delta = _moveVelocity * Time.deltaTime;
+        _parentTransform.position = Vector3.MoveTowards (_parentTransform.position, _currentTargetPos.position, delta);
+        if (Vector3.Distance (_parentTransform.position, _currentTargetPos.position) < 0.001f) {
             //change target
-            if(_currentTargetPos == _startPos){
-                _currentTargetPos = _endPos; 
-                return; 
+            if (_currentTargetPos == _startPos) {
+                _currentTargetPos = _endPos;
+                return;
             }
 
-            _currentTargetPos = _startPos; 
+            _currentTargetPos = _startPos;
         }
     }
 
--- Assets/Scripts/Enemy/Tank.cs
+++ Assets/Scripts/Enemy/Tank.cs
@@ -1,14 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using DG.Tweening; 
 
 public class Land : Enemy {
     [Header ("characteristic")]
     [SerializeField] private float _triggerDistanceFromPlayer;
-    [SerializeField] private float _rotationAnimSmoothness = 0.5f; 
     private Vector3 noRot = new Vector3 (0, 0, 0);
     private Vector3 yesRot = new Vector3 (0, 180, 0);
+    
+    [Header("Animation Parameters")]
+    [SerializeField] private float _rotationAnimSmoothness = 0.5f; 
     // Start is called before the first frame update
     void Start () {
         DOTween.Init(); 
